86dbad7b7c61e49cf86f6efa7b3967683a4be13e
Giovana Zopelaro <giovanaz@ciandt.com>
2025-07-18 17:04:26 -0300

    feat: backbutton component

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Text, StyleSheet, TouchableOpacity } from "react-native";
+import { ArrowLeft } from "lucide-react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const BackButton = () => {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      style={styles.backButton}
+      onPress={() => navigation.goBack()}
+      activeOpacity={0.7}
+    >
+      <ArrowLeft size={20} color="#F8F7F4" />
+      <Text style={styles.backButtonText}>Back</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  backButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+  },
+  backButtonText: {
+    fontSize: 16,
+    color: "#F8F7F4",
+    marginLeft: 8,
+  },
+});
+
+export default BackButton;
